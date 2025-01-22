4d25d2734d240185ef1dfa5770fa5d02bc1863e7
NotKieru <isequielnascimento@gmail.com>
2025-01-22 11:42:56 -0300

    feat(pages): add empty pages for route creation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:aprendoai_front/widgests/bottom_navigation.dart';
+import 'package:aprendoai_front/widgets/bottom_navigation.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    appBar: AppBar(),
-    bottomNavigationBar: BottomNavigation(),
-    
+    return MaterialApp(
+      title: 'Home',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("HOME"),
+        ),
+        body: const Center(
+          child: Text('Home Page'),
+        ),
+      ),
     );
   }
 }
