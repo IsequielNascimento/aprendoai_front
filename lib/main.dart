876d1ca54a7736895af7244d393525e3ddc74d84
NotKieru <isequielnascimento@gmail.com>
2025-01-22 11:49:07 -0300

    update: remove homeScreen default

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:aprendoai_front/pages/home.dart';
 import 'package:flutter/material.dart';
-import 'package:aprendoai_front/widgests/bottom_navigation.dart';
+import 'package:aprendoai_front/widgets/bottom_navigation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +12,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomePage()
+      title: 'Aprendo AI',
+
+      home: BottomNavigation(),
     );
+    
   }
 }
