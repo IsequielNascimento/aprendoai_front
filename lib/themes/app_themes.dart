75029f8abf7f75b607c2d6383c2d619fd503951c
NotKieru <isequielnascimento@gmail.com>
2025-02-04 23:43:26 -0300

    feat: defining main colors

---
+++ lib/themes/app_themes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Appthemes {
+  //Cores
+  static final primary = Color(0xFF05274D);
+  static final background = Color(0xFFFFFFFF);
+  static final stroke = Color(0xFFE3E3E6);
+  static final greySubtitle = Color(0xFF706E7A);
+  static final avaliationColor = Color(0xFFF9B405);
+  static final blueColor = Color(0xFF4062bb);
+  static final blackColor = Color(0xff1a1423);
+
+
+  //images
+
+
+
+}
